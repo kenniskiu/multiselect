261db3174fc6850012ca948bc7bbdd512aeee899
kenniskiu <kenniskiufk123@gmail.com>
2025-02-04 22:04:14 +0700

    feature(app):multiselect

--- src/stories/MultiselectDropdown.stories.js
+++ src/stories/MultiselectDropdown.stories.js
@@ -10,6 +10,15 @@ export default {
   argTypes: {
     isMultiple: { control: "boolean" },
     isWithSearch: { control: "boolean" }, 
+    optionLabels: { 
+      control: "object",
+      defaultValue: [
+        { label: "Option 1", value: "option1" },
+        { label: "Option 2", value: "option2" },
+        { label: "Option 3", value: "option3" }
+      ]
+    },
+    isOutlined: { control: "boolean" }, 
   },
 };
 
